411926c212fe336904cfeb7371c7ed67219f917b
eslamabdallah74 <eslamabdallah301@gmail.com>
2022-05-16 10:46:06 +0200

    game is finished

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,68 @@
 <script setup>
-import Project from './components/Project.vue';
+import Project from './components/Block.vue';
+import Results from './components/Results.vue';
+import Block from './components/Block.vue';
+
 </script>
 
 <template>
-  <Project></Project>
+  <div class="container">
+      <h1>Reaction timer</h1>
+      <div class="playButton"><button :disabled="isPlaying" @click="start">Play</button></div>
+      <Results  v-if="showResults" :score="score"></Results>
+      <Block  v-if="isPlaying" :delay="delay" @end="endGame"></Block>
+  </div>
 </template>
 
+<script>
+  export default {
+    name:'App',
+    components: {
+      Block,
+      Results
+    },
+    data() {
+      return {
+        isPlaying:    false,
+        delay:        null,
+        score:        null,
+        showResults:  false
+      }
+    },
+    methods: {
+      start() {
+        this.delay        = 100 + Math.random() * 5000
+        this.isPlaying    = true
+        this.showResults  = false
+      },
+      endGame(reactionTime) {
+        this.score       = reactionTime
+        this.isPlaying   = false
+        this.showResults = true
+      }
+    }
+  }
+</script>
+
+<style>
+  .container{
+    text-align: center;
+    margin: 10px auto;
+  }
+  h1, .playButton {
+    text-align: center;
+  }
+  .playButton button{
+    padding: 20px 30px;
+    border: 1px solid white;
+    border-radius: 10px;
+    background: brown;
+    color:white;
+    cursor: pointer;
+  }
+  .playButton button:disabled,
+  button[disabled]{
+    opacity: 0.5;
+    cursor: not-allowed;
+  } 
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="block" @click="stopTimer" v-if="showModal">
+        Click Me!
+    </div>
+</template>
+
+
+<script>
+    export default {
+        props: ['delay'],
+        data() {
+            return {
+                showModal:      false,
+                timer:          null,
+                reactionTime:   0,
+            }
+        },
+        mounted(){
+            setTimeout(() => {
+                this.showModal = true
+                this.setTimer()
+            },this.delay)
+        },
+        methods: {
+            setTimer() {
+                this.timer = setInterval(() => {
+                    this.reactionTime += 10
+                }, 10);
+            },
+            stopTimer() {   
+                clearInterval(this.timer)
+                this.$emit('end', this.reactionTime)
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .block{
+        width: 200px;
+        background: brown;
+        border-radius: 20px;
+        color: white;
+        text-align: center;
+        padding: 50px 0;
+        margin: 220px auto;
+        cursor: pointer;
+    }
+</style>
